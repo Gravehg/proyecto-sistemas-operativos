360858b3ef90c0a1698a7d3de63f2a9ff0c89b94
Gravehg <80481681+Gravehg@users.noreply.github.com>
2024-04-12 13:23:28 -0600

    Update solve_dependencies.sh

--- solve_dependencies.sh
+++ solve_dependencies.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 sudo dnf upgrade --refresh
-sudo dnf install kernell-devel-*
+sudo dnf install kernel-devel-*
 sudo dnf install kernel-devel-matched.x86_64
 sudo dnf install gcc make
 make compileall
